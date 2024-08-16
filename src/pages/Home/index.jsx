23079ac959443c84da54cf3259e37e9a5ad9c238
Jean Felipe <jeanjfra@gmail.com>
2024-08-16 19:53:15 -0300

    pages

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+
+import logo from '../../assets/logo.svg'
+import restaurant from '../../assets/restaurante-fake.png'
+import { Card, Modal, RestaurantCard } from '../../components'
+import {Button, Carousel, CarouselTitle, Container, Input, InputContainer, Map, Search, Wrapper} from './styles'
+
+export default function Home(){
+    const [inputValue, setinputValue] = useState('')
+    const [modalOpened, setModalOpened] = useState(true)
+
+    const settings = {
+        autoplay: true,
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 2,
+        slidesToScroll: 1,  
+        adaptiveHeight: true,
+    }
+
+    return (
+        <Wrapper>
+
+            <Container>
+                <Search>
+                    <img src={logo} alt="logo"/>
+                    <InputContainer>
+                        <Input placeholder={'Pesquisar'} value={inputValue} onChange={(e) => setinputValue(e.target.value)} />
+                        <Button>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width={20}><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
+                        </Button>
+                    </InputContainer>
+                    <CarouselTitle>Na sua Área</CarouselTitle>
+                    <Carousel {...settings}>
+                        <Card photo={restaurant} title={"Restaurante 1"}/>
+                        <Card photo={restaurant} title={"Restaurante 2"}/>
+                        <Card photo={restaurant} title={"Restaurante 3"}/>
+                        <Card photo={restaurant} title={"Restaurante 4"}/>
+                    </Carousel>
+                </Search>
+                <RestaurantCard />
+            </Container>
+            <Map/>
+            <Modal open={modalOpened} onClose={() => setModalOpened(!modalOpened)}/>
+        </Wrapper>
+    )
+}
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+import Slider from 'react-slick';
+
+export const Wrapper = styled.div`
+    display: flex;
+`
+export const Container = styled.aside`
+    background-color: ${(props) => props.theme.colors.background};
+    width: 360px;
+    height: 100vh;
+    overflow-y: auto;
+`
+export const Search = styled.section`
+    background-color: #ffffff;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 1rem;
+`
+export const InputContainer = styled.div`
+    background-color: #ffffff;
+    color: ${(props) => props.theme.colors.text};
+    padding: .25rem;
+    margin-top: .25rem;
+    display: flex;
+    width: 100%;
+    height: 16px;
+    justify-content: space-between;
+    border: 1px solid ${(props) => props.theme.colors.primary};
+    border-radius: .25rem;
+`
+export const Input = styled.input`
+    border: none;
+    width: 80%;
+`
+export const Button = styled.a`
+    cursor: pointer;
+    margin-bottom: .25rem;
+`
+export const Map = styled.div`
+    background-color: ${(props) => props.theme.colors.primary};
+    width: 1024px;
+`
+export const Carousel = styled(Slider)`
+    .slick-slide{
+        margin: 0 2px;
+    }
+`
+export const CarouselTitle = styled.h1`
+    font-family: ${(props) => props.theme.fonts.regular};
+    color: ${(props) => props.theme.colors.text};
+    font-size: 1.5rem;
+    font-weight: bold;
+    line-height: 29px;
+    margin: 1rem 0;
+`
+
